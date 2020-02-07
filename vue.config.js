528bf04fa1fca1e2a200a46cf787161caab52bab
Tomas Töws <tomas.toews96@gmx.de>
2020-02-07 22:58:54 +0100

    Added public path to config file

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    baseUrl: '',
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/hellospace/'
+    : '/',
     outputDir: undefined,
     assetsDir: undefined,
     runtimeCompiler: undefined,
